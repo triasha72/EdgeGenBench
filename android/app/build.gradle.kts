534fa3a174481e16437067c12db5f184baad20d0
triasha72 <tsarkar34@gatech.edu>
2026-08-25 10:57:59 -0400

    Publish clearly labeled 16 KiB Android APK

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.edgegenbench"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
         externalNativeBuild {
             cmake {
                 cppFlags += "-std=c++17"
